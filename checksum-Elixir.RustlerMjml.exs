b35332fbd3d9ff418e455d26018f27f8ab599e44
George Lima <georgelima11@hotmail.com>
2022-07-31 15:37:03 -0300

    feat: push checksums

---
+++ checksum-Elixir.RustlerMjml.exs
@@ -0,0 +1,10 @@
+%{
+  "libmjml-v0.0.4-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:db413b6cd336e4cd58b7fd315caf457c5f8501217432b1fdcd74f17caf4a692b",
+  "libmjml-v0.0.4-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:90ec0a5f2ab24801794834a57b91d57401191262a50e9db67e41bec12952967b",
+  "libmjml-v0.0.4-nif-2.16-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:d3846dc267771081782fa105b042034dbd2a1ce30aac616d7aaf51b84eec2eb5",
+  "libmjml-v0.0.4-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:6b16d931d3d127e15035961bd9f101f27679de67ed417008ce2c4fe613a87951",
+  "libmjml-v0.0.4-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:bd254835e648ed9ff2eb86f5b0d8672e6c38d39bd19008ecb977cca928b33f11",
+  "libmjml-v0.0.4-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:8415921f267cd417a683e481b029cdb2195f8049461e70ae5d93dc51ed1e0460",
+  "mjml-v0.0.4-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:ad322ad2a4207c73274d052710f867c3c3ead0c5e612a658ce452a774575a407",
+  "mjml-v0.0.4-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:b3f6be62bb0e30fdb0306898961deeb7b735275b7a812b5e57922fe63ee13d9d",
+}
